9add8b17e27d5b2dfa4799f4b1c147c8291484f5
theetchana07 <theetchanarevathi@gmail.com>
2026-07-24 21:26:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {
   Home, BookOpen, TrendingUp, Users, Calendar, User, 
   Sun, Moon, CheckCircle, ShieldCheck, Terminal, ChevronDown, ChevronUp, Briefcase, GraduationCap, Sparkles
 } from 'lucide-react';
+import myPhoto from './assets/my-photo.png';
+import gemPoster from './assets/gem.png';
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(true);
