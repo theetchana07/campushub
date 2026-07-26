abfa166945fa7ff499f15426ab93a142dff5aa4e
theetchana07 <theetchanarevathi@gmail.com>
2026-07-26 11:11:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {
   Home, BookOpen, TrendingUp, Users, Calendar, User,
   Sun, Moon, CheckCircle, ShieldCheck, Terminal, ChevronDown, ChevronUp, Briefcase, GraduationCap, Sparkles
 } from 'lucide-react';
-import tadaPoster from './tada.png';
+import tadaPoster from './assets/tada.png';
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(true);
